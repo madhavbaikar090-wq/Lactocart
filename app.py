ab5f4642818802b7e6893725e8d1be0ae55f9071
madhavbaikar090-wq <madhavbaikar090@gmail.com>
2026-03-01 13:47:33 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form.get('username')
+    return f"""
+    <div style="text-align:center; margin-top:100px; font-family:Arial;">
+        <h1 style="color:#ff9800;">Welcome {username} 🐄🥛</h1>
+        <a href="/" style="text-decoration:none; font-size:18px;">Go Back</a>
+    </div>
+    """
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
